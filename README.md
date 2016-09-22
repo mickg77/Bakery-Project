2ad10bd76c920a246934fefb26d3012690da16cf
Michael Griffiths <mickg77@hotmail.com>
2016-09-22 08:30:37 +0000

    mobile friendly

--- README.md
+++ README.md
@@ -6,21 +6,13 @@
     ----------------------------------------------------------------- 
 
 
-Hi there! Welcome to Cloud9 IDE!
+Changes:
 
-To get you started, we included a small hello world application.
-
-1) Open the hello-world.html file
-
-2) Click on the Preview button to open a live preview pane
-
-3) Make some changes to the file, save, watch the preview, and have fun!
-
-Happy coding!
-The Cloud9 IDE team
-
-
-## Support & Documentation
-
-Visit http://docs.c9.io for documentation, or http://support.c9.io for support.
-To watch some training videos, visit http://www.youtube.com/user/c9ide
+Added favicon to images folder
+Added favicon code to head of both documents
+Added Login to Menu
+Created login.php
+Created a dropdown menu
+Straightened out margins
+Made site mobile friendly
+Removed Contact file as it is unnecessary at the moment.
---
+++ login.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<!-- a document created by Michael Griffiths-->
+<html lang="en">
+    <head>
+        <title>Michael's Cake Shop </title>
+        <!--viewport sets up the site to fill the mobile screen-->
+        
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+       <link rel="stylesheet" href="css/mystyles.css" type="text/css" />
+<link href='https://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Indie+Flower' rel='stylesheet' type='text/css'>
+<link rel="icon" type="image/x-icon" href="images/favicon.ico" />
+    </head>
+    <body>
+        <header><h1>The Bakery</h1></header>
+    <nav>
+        <h2>Menu</h2>
+        <ul>
+  <li><a href='index.html'>Home</a> </li>   
+  <li><a href='#'>Pastries</a></li> 
+  <li><a href="#">Doughnuts</a> </li> 
+  <li><a href='#'>Pies</a></li> 
+  <li><a href='contact.html'>Contact</a></li> 
+<li><a href='login.php'>Login</a></li> 
+    </ul>
+</nav>
+  <form name="myform" action="login.php" method="POST">
+      
+     
+      <table>
+          
+          <th> 
+          <td colspan="2"><h2>Login</h2></td>
+          </th>
+          <tr>
+              <td>   <label>Username</label>      </td>
+              <td>      <input type="text" required>     </td>
+          </tr>
+            <tr>
+              <td>      <label>Password</label>    </td>
+              <td>       <input type="password" required>   </td>
+          </tr>
+          <tr>
+              <td colspan="2"><input type="submit" name="btn" class="btn-style" value="submit" /></td>
+             
+          </tr>
+          
+      </table>
+     
+     
+      
+      
+      
+  </form>
+<div id="aside">
+    <h2>Some other stuff...</h2>
+    <p>There can be a wee bit of content in here</p>
+    
+</div>
+</body>
+</html>
